be0e2d5debc5235b15f044724ad88e7978aa47b6
mis-kcn <kcniverba@myinternetsupport.com>
2023-02-20 01:07:59 +0800

    feat: add helper and validator for import file

--- resources/views/company/partials/update-company-information-form.blade.php
+++ resources/views/company/partials/update-company-information-form.blade.php
@@ -43,6 +43,7 @@
             <x-input-label for="logo" :value="__('Logo')" />
             <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none" id="logo" type="file" name="logo">
             <x-input-error class="mt-2" :messages="$errors->get('logo')" />
+            <p class="mt-1 text-sm text-gray-500" id="file_input_help">PNG, JPG, JPEG only. Max file size of 5MB.</p>
         </div>
 
         <div class="flex items-center gap-4">
--- resources/views/employees/partials/import-employees.blade.php
+++ resources/views/employees/partials/import-employees.blade.php
@@ -8,11 +8,21 @@
             {{ __('Batch import employees from CSV/XLSX format') }}
         </p>
     </header>
+    <br>
+    <a href="#">
+        <x-primary-button class="bg-blue-800">Download sample template</x-primary-button>
+    </a>
 
-    <form method="post" action="{{ route('tenant.company.addManager') }}" class="mt-6 space-y-6">
+    <form method="post" action="{{ route('tenant.employees.import') }}" class="mt-6 space-y-6">
         @csrf
         @method('post')
 
+        <div>
+            <x-input-label for="file" :value="__('File')" />
+            <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none" id="file" type="file" name="file">
+            <x-input-error class="mt-2" :messages="$errors->get('file')" />
+            <p class="mt-1 text-sm text-gray-500" id="file_input_help">XLS, XLSX, and CSV only. Max file size of 10MB.</p>
+        </div>
 
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Import') }}</x-primary-button>
