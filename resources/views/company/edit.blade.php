6982454510f667957bd3535611de570a59bbfbb6
mis-kcn <kcniverba@myinternetsupport.com>
2023-02-19 21:22:14 +0800

    feat: optimize spacing company page

--- resources/views/company/edit.blade.php
+++ resources/views/company/edit.blade.php
@@ -22,6 +22,14 @@
             </div>
         </div>
     </div>
+
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                @include('company.partials.add-manager')
+            </div>
+        </div>
+    </div>
     
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
